7841c149d3d0d2e740a222133aedc18ed36f1bce
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-09-25 14:34:38 -0400

    Update sm64-MacOS.sh

--- sm64-MacOS.sh
+++ sm64-MacOS.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-alias spin='while :; do for s in ◐◓◑◒; do printf "\r$s"; sleep .1; done; done'
-
 spinner_pid=
 function start_spinner {
     set +m
-    spin &
+    echo -n "         "
+    { while : ; do for X in '◐' '◓' '◑' '◒'; do echo -en "\r[$X] $1" ; sleep 0.1 ; done ; done & } 2>/dev/null
     spinner_pid=$!
 }
 
 function stop_spinner {
-    kill -9 $spinner_pid
+    { kill -9 $spinner_pid && wait; } 2>/dev/null
     set -m
+    echo -en "\033[2K\r"
 }
+
+trap stop_spinner EXIT
 
 clear
 
