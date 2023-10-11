e4cc706a8c5c8f3aeac9d5fed250b784819bd417
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-10-11 13:46:14 -0400

    Update zelda3-MacOS.sh

--- zelda3/zelda3-MacOS.sh
+++ zelda3/zelda3-MacOS.sh
@@ -90,4 +90,118 @@ read path
 
 cp "$path" ~/zelda3/zelda3.sfc
 
-CFLAGS="-Wno-deprecated-non-protoype" make
+echo "
+***************************************
+              building
+***************************************
+"
+
+start_spinner "building"
+CFLAGS="-Wno-deprecated-non-protoype" make > /dev/null 2>&1
+stop_spinner
+
+clear
+
+echo "
+***************************************
+               packing
+***************************************
+"
+
+start spinner "packing"
+mkdir -p ~/zelda3.app/Contents/MacOS
+
+echo '#!/bin/bash
+abspath ()
+{
+case "${1}" in
+    [./]*)
+    local ABSPATH="$(cd ${1%/*}; pwd)/"
+    echo "${ABSPATH/\/\///}"
+    ;;
+    *)
+    echo "${PWD}/"
+    ;;
+esac
+}
+
+
+CURRENTPATH=`abspath ${0}`
+
+cd $CURRENTPATH
+cd ../Resources
+./zelda3' >> ~/zelda3.app/Contents/MacOS/zelda3
+
+cd ~
+mv ~/zelda3/zelda3 ~/zelda3.app/Contents/Resources/zelda3
+mv ~/zelda3/zelda3.ini ~/zelda3.app/Contents/Resources/
+mv ~/zelda3/zelda3_assets.dat ~/zelda3.app/Contents/Resources/
+
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	<string>en</string>
+	<key>CFBundleDisplayName</key>
+	<string>Zelda3</string>
+	<key>CFBundleExecutable</key>
+	<string>zelda3</string>
+	<key>CFBundleIconFile</key>
+	<string>zelda3.icns</string>
+	<key>CFBundleIdentifier</key>
+	<string>org.'$USER'.Zelda3</string>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundleName</key>
+	<string>Zelda3</string>
+	<key>CFBundlePackageType</key>
+	<string>APPL</string>
+	<key>CFBundleShortVersionString</key>
+	<string>1.0</string>
+	<key>LSMinimumSystemVersion</key>
+	<string>10.11.0</string>
+	<key>LSUIElement</key>
+	<false/>
+	<key>NSAppTransportSecurity</key>
+	<dict>
+		<key>NSAllowsArbitraryLoads</key>
+		<true/>
+	</dict>
+	<key>NSHumanReadableCopyright</key>
+	<string>© 2023 '$USER'</string>
+	<key>NSMainNibFile</key>
+	<string>MainMenu</string>
+	<key>NSPrincipalClass</key>
+	<string>NSApplication</string>
+</dict>
+</plist>' >> Info.plist
+
+mv Info.plist ~/sm64.app/Contents/
+
+wget https://github.com/Eclipse-5214/sm64-MacOS/raw/main/zelda3/zelda3.icns
+
+mv zelda3.icns ~/zelda3.app/Contents/Resources
+stop_spinner
+
+clear
+
+echo "
+***************************************
+               Cleaning
+***************************************
+
+This needs root to do
+"
+
+sudo rm -r ~/zelda3
+
+clear
+
+echo "
+***************************************
+                 Done
+***************************************
+
+Finished app should be in home directory"
